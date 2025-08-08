598c32f7f2c2a353d0e69409016a905cd3c7986c
pineappletapped <rjwglobal@googlemail.com>
2025-08-08 17:51:28 +0100

    Fix scoreboard overlay syntax error

--- wwwroot/app/js/main-overlay.js
+++ wwwroot/app/js/main-overlay.js
@@ -1372,18 +1372,22 @@ function renderOverlayFromFirebase(state, graphics, branding) {
             }
             hb.innerHTML = `<div class='lower-third-default' style='position:absolute;bottom:2rem;left:50%;transform:translateX(-50%);font-family:${branding.font};'>Highest Break: ${scoreboardData.highBreak}</div>`;
         } else {
-            overlayContainer.querySelector('#high-break')?.remove();
+            const hb = overlayContainer.querySelector('#high-break');
+            if(hb) hb.remove();
         }
-    } else if (scoreboardOverlay && prevScoreboardVisible) {
-        playTransition(scoreboardOverlay,'out',prevScoreboardData?.transitionOut);
-        if(prevScoreboardVisible){
-            const topSp = sponsorsData[sponsorPlacements.scoreboardTop];
-            const bottomSp = sponsorsData[sponsorPlacements.scoreboardBottom];
-            if(topSp) addSponsorLog(eventId,{ts:Date.now(),placement:'scoreboardTop',sponsor:sponsorPlacements.scoreboardTop,action:'hide'});
-            if(bottomSp) addSponsorLog(eventId,{ts:Date.now(),placement:'scoreboardBottom',sponsor:sponsorPlacements.scoreboardBottom,action:'hide'});
+    } else {
+        if (scoreboardOverlay && prevScoreboardVisible) {
+            playTransition(scoreboardOverlay,'out',prevScoreboardData?.transitionOut);
+            if(prevScoreboardVisible){
+                const topSp = sponsorsData[sponsorPlacements.scoreboardTop];
+                const bottomSp = sponsorsData[sponsorPlacements.scoreboardBottom];
+                if(topSp) addSponsorLog(eventId,{ts:Date.now(),placement:'scoreboardTop',sponsor:sponsorPlacements.scoreboardTop,action:'hide'});
+                if(bottomSp) addSponsorLog(eventId,{ts:Date.now(),placement:'scoreboardBottom',sponsor:sponsorPlacements.scoreboardBottom,action:'hide'});
+            }
+            scoreboardOverlay = null;
+            const hb = overlayContainer.querySelector('#high-break');
+            if(hb) hb.remove();
         }
-        scoreboardOverlay = null;
-        overlayContainer.querySelector('#high-break')?.remove();
     }
     prevScoreboardVisible = scoreboardShow;
     prevScoreboardData = scoreboardData;
